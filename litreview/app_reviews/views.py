eb5fd19c37f502369b91d0d354a8cca00f456f03
Mahery Gonin <mahery.gonin@gmail.com>
2021-06-03 11:00:55 +0200

    Hide pages when user is logout

--- litreview/app_reviews/views.py
+++ litreview/app_reviews/views.py
@@ -6,147 +6,172 @@ from .models import Review, Ticket
 # Create your views here.
 def ticket_create(request):
 
-    if request.method == 'POST':
-        form = RawCreateTicketForm(request.POST, request.FILES)
-        if form.is_valid:
-            Ticket.objects.create(
-                title= request.POST['title'],
-                description= request.POST['description'],
-                user= request.user,
-                image= request.FILES.get('image', 'image/upload/NULL.jpg')
-            )
-            return redirect('/')
+    if request.user.is_authenticated == False:
+        return redirect('login')
     else:
-        form = RawCreateTicketForm()
+        if request.method == 'POST':
+            form = RawCreateTicketForm(request.POST, request.FILES)
+            if form.is_valid:
+                Ticket.objects.create(
+                    title= request.POST['title'],
+                    description= request.POST['description'],
+                    user= request.user,
+                    image= request.FILES.get('image', 'image/upload/NULL.jpg')
+                )
+                return redirect('/')
+        else:
+            form = RawCreateTicketForm()
 
-    context = {
-        'form': form,
-    }
-    return render(request, 'createticket.html', context)
+        context = {
+            'form': form,
+        }
+        return render(request, 'createticket.html', context)
+
 
 
 def ticket_modify(request, id):
 
-    ticket_modify = Ticket.objects.get(pk=id)
-
-    if request.method == 'POST':
-        form = RawCreateTicketForm(request.POST, request.FILES)
-        if form.is_valid:
-            Ticket.objects.filter(pk=id).update(
-                title= request.POST['title'],
-                description= request.POST['description'],
-                image= request.FILES.get('image', ticket_modify.image)
-            )
-            return redirect('/')
+    if request.user.is_authenticated == False:
+        return redirect('login')
     else:
-        form = RawCreateTicketForm()
 
-        form.fields['title'].initial = ticket_modify.title
-        form.fields['description'].initial = ticket_modify.description
-        form.fields['image'].initial = ticket_modify.image
+        ticket_modify = Ticket.objects.get(pk=id)
 
-    context = {
-        'form': form,
-        'ticket_infos': ticket_modify,
-    }
+        if request.method == 'POST':
+            form = RawCreateTicketForm(request.POST, request.FILES)
+            if form.is_valid:
+                Ticket.objects.filter(pk=id).update(
+                    title= request.POST['title'],
+                    description= request.POST['description'],
+                    image= request.FILES.get('image', ticket_modify.image)
+                )
+                return redirect('/')
+        else:
+            form = RawCreateTicketForm()
 
-    return render(request, 'modifyticket.html', context)
+            form.fields['title'].initial = ticket_modify.title
+            form.fields['description'].initial = ticket_modify.description
+            form.fields['image'].initial = ticket_modify.image
+
+        context = {
+            'form': form,
+            'ticket_infos': ticket_modify,
+        }
+
+        return render(request, 'modifyticket.html', context)
 
 
 def review_create(request):
 
-    if request.method == 'POST':
-        form_review = RawCreateReviewForm(request.POST)
-        form_ticket = RawCreateTicketForm(request.POST, request.FILES)
-        if form_review.is_valid and form_ticket.is_valid:
-            new_ticket = Ticket.objects.create(
-                title= request.POST['title'],
-                description= request.POST['description'],
-                user= request.user,
-                image= request.FILES.get('image', 'image/upload/NULL.jpg')
-            )
-            Review.objects.create(
-                headline= request.POST['headline'],
-                body= request.POST['body'],
-                user= request.user,
-                ticket= new_ticket,
-                rating= request.POST['rating'],
-            )
-        return redirect('/')
-
+    if request.user.is_authenticated == False:
+        return redirect('login')
     else:
-        form_review = RawCreateReviewForm()
-        form_ticket = RawCreateTicketForm()
+        if request.method == 'POST':
+            form_review = RawCreateReviewForm(request.POST)
+            form_ticket = RawCreateTicketForm(request.POST, request.FILES)
+            if form_review.is_valid and form_ticket.is_valid:
+                new_ticket = Ticket.objects.create(
+                    title= request.POST['title'],
+                    description= request.POST['description'],
+                    user= request.user,
+                    image= request.FILES.get('image', 'image/upload/NULL.jpg')
+                )
+                Review.objects.create(
+                    headline= request.POST['headline'],
+                    body= request.POST['body'],
+                    user= request.user,
+                    ticket= new_ticket,
+                    rating= request.POST['rating'],
+                )
+            return redirect('/')
 
-    context = {
-        'form_review': form_review,
-        'form_ticket': form_ticket,
-    }
+        else:
+            form_review = RawCreateReviewForm()
+            form_ticket = RawCreateTicketForm()
 
-    return render(request, 'createreview.html', context)
+        context = {
+            'form_review': form_review,
+            'form_ticket': form_ticket,
+        }
+
+        return render(request, 'createreview.html', context)
 
 def review_create_reply(request, id):
 
-    old_ticket = Ticket.objects.get(id=id)
-
-    if request.method == 'POST':
-        form_review = RawCreateReviewForm(request.POST)
-        form_ticket = RawCreateTicketForm(request.POST, request.FILES)
-        if form_review.is_valid and form_ticket.is_valid:
-            Review.objects.create(
-                headline= request.POST['headline'],
-                body= request.POST['body'],
-                user= request.user,
-                ticket= old_ticket,
-                rating= request.POST['rating'],
-            )
-        return redirect('/')
+    if request.user.is_authenticated == False:
+        return redirect('login')
     else:
-        form_review = RawCreateReviewForm()
+        old_ticket = Ticket.objects.get(id=id)
 
-    context = {
-        'form_review': form_review,
-        'infos_ticket': old_ticket,
-    }
-    return render(request, 'replycreatereview.html', context)
+        if request.method == 'POST':
+            form_review = RawCreateReviewForm(request.POST)
+            form_ticket = RawCreateTicketForm(request.POST, request.FILES)
+            if form_review.is_valid and form_ticket.is_valid:
+                Review.objects.create(
+                    headline= request.POST['headline'],
+                    body= request.POST['body'],
+                    user= request.user,
+                    ticket= old_ticket,
+                    rating= request.POST['rating'],
+                )
+            return redirect('/')
+        else:
+            form_review = RawCreateReviewForm()
+
+        context = {
+            'form_review': form_review,
+            'infos_ticket': old_ticket,
+        }
+        return render(request, 'replycreatereview.html', context)
 
 
 def review_modify(request, id):
-    review_modify = Review.objects.get(pk=id)
 
-    if request.method == 'POST':
-        form = RawCreateReviewForm(request.POST)
-        if form.is_valid:
-            Review.objects.filter(pk=id).update(
-                headline= request.POST['headline'],
-                body= request.POST['body'],
-                rating= request.POST['rating'],
-            )
-            return redirect('/')
+    if request.user.is_authenticated == False:
+        return redirect('login')
     else:
-        form = RawCreateReviewForm()
+        review_modify = Review.objects.get(pk=id)
 
-        form.fields['headline'].initial = review_modify.headline
-        form.fields['body'].initial = review_modify.body
-        form.fields['rating'].initial = review_modify.rating
+        if request.method == 'POST':
+            form = RawCreateReviewForm(request.POST)
+            if form.is_valid:
+                Review.objects.filter(pk=id).update(
+                    headline= request.POST['headline'],
+                    body= request.POST['body'],
+                    rating= request.POST['rating'],
+                )
+                return redirect('/')
+        else:
+            form = RawCreateReviewForm()
 
-    context = {
-        'form': form,
-        'review_infos': review_modify,
-    }
-    return render(request, 'modifyreview.html', context)
+            form.fields['headline'].initial = review_modify.headline
+            form.fields['body'].initial = review_modify.body
+            form.fields['rating'].initial = review_modify.rating
+
+        context = {
+            'form': form,
+            'review_infos': review_modify,
+        }
+        return render(request, 'modifyreview.html', context)
 
 
 
 def posts(request):
-    return render(request, 'posts.html')
+
+    if request.user.is_authenticated == False:
+        return redirect('login')
+    else:
+        return render(request, 'posts.html')
 
 def feed(request):
+    
+    if request.user.is_authenticated == False:
+        return redirect('login')
+    else:
+        tickets = [
+            {'id': 1, 'title': 'Titre du livre 1', 'body': 'Description 1'},
+            {'id': 2, 'title': 'Titre du livre 2', 'body': 'Description 2'},
+            {'id': 3, 'title': 'Titre du livre 3', 'body': 'Description 3'},
+        ]
 
-    tickets = [
-        {'id': 1, 'title': 'Titre du livre 1', 'body': 'Description 1'},
-        {'id': 2, 'title': 'Titre du livre 2', 'body': 'Description 2'},
-        {'id': 3, 'title': 'Titre du livre 3', 'body': 'Description 3'},
-    ]
-
-    return render(request, 'flux.html', {'tickets': tickets})
+        return render(request, 'flux.html', {'tickets': tickets})
--- litreview/app_subs/views.py
+++ litreview/app_subs/views.py
@@ -7,40 +7,47 @@ from django.db import IntegrityError
 
 def home(request):
 
-    if request.method == 'POST':
-        form = FollowUser(request.POST)
-        if form.is_valid:
-            if request.user == User.objects.get(username=request.POST['followed_user']):
-                messages.error(request, 'Vous ne pouvez pas vous ajouter vous même.')
-            else:
-                try:
-                    UserFollows.objects.create(
-                        user = request.user,
-                        followed_user = User.objects.get(username=request.POST['followed_user']),
-                    )
-                except IntegrityError:
-                    messages.error(request, 'Vous avez déjà ajouter cet utilisateur.')
-
+    if request.user.is_authenticated == False:
+        return redirect('login')
     else:
-        form = FollowUser()
+        if request.method == 'POST':
+            form = FollowUser(request.POST)
+            if form.is_valid:
+                if request.user == User.objects.get(username=request.POST['followed_user']):
+                    messages.error(request, 'Vous ne pouvez pas vous ajouter vous même.')
+                else:
+                    try:
+                        UserFollows.objects.create(
+                            user = request.user,
+                            followed_user = User.objects.get(username=request.POST['followed_user']),
+                        )
+                    except IntegrityError:
+                        messages.error(request, 'Vous avez déjà ajouter cet utilisateur.')
 
-    following_list = []
-    followers_list = []
+        else:
+            form = FollowUser()
 
-    for f in UserFollows.objects.filter(user_id=request.user):
-        following_list.append(User.objects.get(id=f.followed_user_id))
+        following_list = []
+        followers_list = []
 
-    for f in UserFollows.objects.filter(followed_user_id=request.user):
-        followers_list.append(User.objects.get(id=f.user_id))
+        for f in UserFollows.objects.filter(user_id=request.user):
+            following_list.append(User.objects.get(id=f.followed_user_id))
 
-    context= {
-        'form': form,
-        'following': following_list,
-        'followers': followers_list,
-    }
+        for f in UserFollows.objects.filter(followed_user_id=request.user):
+            followers_list.append(User.objects.get(id=f.user_id))
 
-    return render(request, 'subs.html', context)
+        context= {
+            'form': form,
+            'following': following_list,
+            'followers': followers_list,
+        }
+
+        return render(request, 'subs.html', context)
 
 def unsubscribe(request, id):
-    UserFollows.objects.filter(user_id=request.user).filter(followed_user_id=id).delete()
-    return redirect('subs')
+
+    if request.user.is_authenticated == False:
+        return redirect('login')
+    else:
+        UserFollows.objects.filter(user_id=request.user).filter(followed_user_id=id).delete()
+        return redirect('subs')
