488fd54a8fdd9449572603b29acc0b10ea943de0
Mahery Gonin <mahery.gonin@gmail.com>
2021-06-03 14:20:41 +0200

    Change access for modify ticket/review

--- litreview/app_reviews/views.py
+++ litreview/app_reviews/views.py
@@ -37,28 +37,31 @@ def ticket_modify(request, id):
 
         ticket_modify = Ticket.objects.get(pk=id)
 
-        if request.method == 'POST':
-            form = RawCreateTicketForm(request.POST, request.FILES)
-            if form.is_valid:
-                Ticket.objects.filter(pk=id).update(
-                    title= request.POST['title'],
-                    description= request.POST['description'],
-                    image= request.FILES.get('image', ticket_modify.image)
-                )
-                return redirect('/')
+        if ticket_modify.user_id == request.user.id:
+            if request.method == 'POST':
+                form = RawCreateTicketForm(request.POST, request.FILES)
+                if form.is_valid:
+                    Ticket.objects.filter(pk=id).update(
+                        title= request.POST['title'],
+                        description= request.POST['description'],
+                        image= request.FILES.get('image', ticket_modify.image)
+                    )
+                    return redirect('/')
+            else:
+                form = RawCreateTicketForm()
+
+                form.fields['title'].initial = ticket_modify.title
+                form.fields['description'].initial = ticket_modify.description
+                form.fields['image'].initial = ticket_modify.image
+
+            context = {
+                'form': form,
+                'ticket_infos': ticket_modify,
+            }
+
+            return render(request, 'modifyticket.html', context)
         else:
-            form = RawCreateTicketForm()
-
-            form.fields['title'].initial = ticket_modify.title
-            form.fields['description'].initial = ticket_modify.description
-            form.fields['image'].initial = ticket_modify.image
-
-        context = {
-            'form': form,
-            'ticket_infos': ticket_modify,
-        }
-
-        return render(request, 'modifyticket.html', context)
+            return redirect('/')
 
 
 def review_create(request):
@@ -101,28 +104,35 @@ def review_create_reply(request, id):
     if request.user.is_authenticated == False:
         return redirect('login')
     else:
-        old_ticket = Ticket.objects.get(id=id)
+        print(len(Review.objects.filter(ticket_id=id).filter(user_id=request.user.id)))
+        if len(Review.objects.filter(ticket_id=id).filter(user_id=request.user.id)) >= 1:
 
-        if request.method == 'POST':
-            form_review = RawCreateReviewForm(request.POST)
-            form_ticket = RawCreateTicketForm(request.POST, request.FILES)
-            if form_review.is_valid and form_ticket.is_valid:
-                Review.objects.create(
-                    headline= request.POST['headline'],
-                    body= request.POST['body'],
-                    user= request.user,
-                    ticket= old_ticket,
-                    rating= request.POST['rating'],
-                )
             return redirect('/')
-        else:
-            form_review = RawCreateReviewForm()
 
-        context = {
-            'form_review': form_review,
-            'infos_ticket': old_ticket,
-        }
-        return render(request, 'replycreatereview.html', context)
+        else:
+
+            old_ticket = Ticket.objects.get(id=id)
+
+            if request.method == 'POST':
+                form_review = RawCreateReviewForm(request.POST)
+                form_ticket = RawCreateTicketForm(request.POST, request.FILES)
+                if form_review.is_valid and form_ticket.is_valid:
+                    Review.objects.create(
+                        headline= request.POST['headline'],
+                        body= request.POST['body'],
+                        user= request.user,
+                        ticket= old_ticket,
+                        rating= request.POST['rating'],
+                    )
+                return redirect('/')
+            else:
+                form_review = RawCreateReviewForm()
+
+            context = {
+                'form_review': form_review,
+                'infos_ticket': old_ticket,
+            }
+            return render(request, 'replycreatereview.html', context)
 
 
 def review_modify(request, id):
@@ -132,28 +142,30 @@ def review_modify(request, id):
     else:
         review_modify = Review.objects.get(pk=id)
 
-        if request.method == 'POST':
-            form = RawCreateReviewForm(request.POST)
-            if form.is_valid:
-                Review.objects.filter(pk=id).update(
-                    headline= request.POST['headline'],
-                    body= request.POST['body'],
-                    rating= request.POST['rating'],
-                )
-                return redirect('/')
+        if review_modify.user_id == request.user.id:
+            if request.method == 'POST':
+                form = RawCreateReviewForm(request.POST)
+                if form.is_valid:
+                    Review.objects.filter(pk=id).update(
+                        headline= request.POST['headline'],
+                        body= request.POST['body'],
+                        rating= request.POST['rating'],
+                    )
+                    return redirect('/')
+            else:
+                form = RawCreateReviewForm()
+
+                form.fields['headline'].initial = review_modify.headline
+                form.fields['body'].initial = review_modify.body
+                form.fields['rating'].initial = review_modify.rating
+
+            context = {
+                'form': form,
+                'review_infos': review_modify,
+            }
+            return render(request, 'modifyreview.html', context)
         else:
-            form = RawCreateReviewForm()
-
-            form.fields['headline'].initial = review_modify.headline
-            form.fields['body'].initial = review_modify.body
-            form.fields['rating'].initial = review_modify.rating
-
-        context = {
-            'form': form,
-            'review_infos': review_modify,
-        }
-        return render(request, 'modifyreview.html', context)
-
+            return redirect('/')
 
 
 def posts(request):
