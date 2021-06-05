df59cdb550913da0f9808afe99d5c9c79858ef14
Mahery Gonin <mahery.gonin@gmail.com>
2021-06-05 09:01:26 +0200

    Hide register page if user is logged

--- litreview/app_accounts/views.py
+++ litreview/app_accounts/views.py
@@ -5,18 +5,21 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('login')
+    if request.user.is_authenticated:
+        return redirect('/')
     else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+                return redirect('login')
+        else:
+            form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
     
 
 def disconnect(request):
